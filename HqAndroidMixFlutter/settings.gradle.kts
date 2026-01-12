2ebb51124c7ecf87e0b8a62f4bc8a604fc849971
hehuiqi <>
2026-01-12 11:15:40 +0800

    修改安装flutter下载环境配置

--- HqAndroidMixFlutter/settings.gradle.kts
+++ HqAndroidMixFlutter/settings.gradle.kts
@@ -1,5 +1,7 @@
 pluginManagement {
     repositories {
+        val storageUrl: String = System.getenv("FLUTTER_STORAGE_BASE_URL") ?: "https://storage.googleapis.com"
+
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -9,17 +11,21 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
-        maven("https://storage.googleapis.com/download.flutter.io")
+        //maven("https://storage.googleapis.com/download.flutter.io")
+        maven("$storageUrl/download.flutter.io")
     }
 }
 dependencyResolutionManagement {
 //    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 //    将 Gradle 的仓库策略从「强制优先 settings」改为「允许 project 仓库」，兼容 Flutter 插件的仓库添加行为
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    val storageUrl: String = System.getenv("FLUTTER_STORAGE_BASE_URL") ?: "https://storage.googleapis.com"
     repositories {
         google()
         mavenCentral()
-        maven("https://storage.googleapis.com/download.flutter.io")
+//        maven("https://storage.googleapis.com/download.flutter.io")
+        maven("$storageUrl/download.flutter.io")
+
     }
 
 }
